29d8b7fc113176dc084e3ca0cbea5e28d786ac14
Freddie Schultz <freddieschultz+devacademy@gmail.com>
2025-03-24 18:24:59 +1300

    Add routes files to server.ts

--- server/server.ts
+++ server/server.ts
@@ -3,9 +3,14 @@ import * as Path from 'node:path'
 import express from 'express'
 import { preprocessCSS } from 'vite'
 
+import workoutsRoutes from './routes/workouts.ts'
+import exercisesRoutes from './routes/exercises.ts'
+
 const server = express()
 
 server.use(express.json())
+server.use('/api/v1/workouts', workoutsRoutes)
+server.use('/api/v1/exercises', exercisesRoutes)
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
