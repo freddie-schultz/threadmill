08eaeabb83b010ab1dbfc54a357e090ea7e28ce1
Freddie Schultz <freddieschultz+devacademy@gmail.com>
2025-03-24 18:23:17 +1300

    Create exercises.ts routes file

--- server/routes/exercises.ts
+++ server/routes/exercises.ts
@@ -1,6 +1,56 @@
 import express from 'express'
 
-import * as db from '../db/connection.ts'
+import * as db from '../db/functions/exercises.ts'
 
 const router = express.Router()
 export default router
+
+router.get('/', async (req, res, next) => {
+  try {
+    const exercises = await db.getAllExercises()
+    res.json(exercises)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const id = Number(req.params.id)
+    const exercise = await db.getExerciseById(id)
+    res.json(exercise)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newExercise = req.body
+    const response = await db.addExercise(newExercise)
+    res.status(201).json(response)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.patch('/:id', async (req, res, next) => {
+  try {
+    const id = Number(req.params.id)
+    const updatedExercise = req.body
+    const response = await db.updateExercise(id, updatedExercise)
+    res.status(204).json(response)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const id = Number(req.params.id)
+    const response = await db.deleteExercise(id)
+    res.status(204).json(response)
+  } catch (error) {
+    next(error)
+  }
+})
