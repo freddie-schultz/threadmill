aece469a1f22ddb3c4a790d9fae219018a7508f7
Freddie Schultz <freddieschultz+devacademy@gmail.com>
2025-03-24 18:06:42 +1300

    Create workouts.ts database functions

---
+++ server/db/functions/exercises.ts
@@ -0,0 +1,2 @@
+import database from '../connection.ts'
+import { Exercise, ExerciseData } from '../../../models/exercises.ts'
--- server/db/functions/workouts.ts
+++ server/db/functions/workouts.ts
@@ -1,1 +1,32 @@
 import database from '../connection.ts'
+import { Workout, WorkoutData } from '../../../models/workouts.ts'
+
+export async function getAllWorkouts(): Promise<Workout[]> {
+  const results = await database('workouts').select()
+
+  return results as Workout[]
+}
+
+export async function getWorkoutById(id: number): Promise<Workout> {
+  const results = await database('workouts').where('id', id).select().first()
+
+  return results as Workout
+}
+
+export async function addWorkout(newWorkout: WorkoutData): Promise<number> {
+  const results = await database('workouts').insert(newWorkout)
+
+  return results[0]
+}
+
+export async function updateWorkout(id: number, updatedWorkout: WorkoutData): Promise<number> {
+  const results = await database('workouts').where('id', id).update(updatedWorkout)
+
+  return results
+}
+
+export async function deleteWorkout(id: number): Promise<number> {
+  const results = await database('workouts').where('id', id).delete()
+
+  return results
+}
