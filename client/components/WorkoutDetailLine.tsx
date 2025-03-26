2884ec7b1791fe7c0d9f66295ae0ab5b89bce8cd
Freddie Schultz <freddieschultz+devacademy@gmail.com>
2025-03-26 18:39:50 +1300

    Remove unnecessary keys from WorkoutDetailLine

--- client/components/WorkoutDetailLine.tsx
+++ client/components/WorkoutDetailLine.tsx
@@ -3,17 +3,13 @@ import { ExerciseInWorkout } from '../../models/exercises'
 
 export default function WorkoutDetailLine(props: ExerciseInWorkout) {
   return (
-    <table style={{ fontSize: 12, marginTop: 5, marginBottom: 15 }} key={`table${props.id}`}>
-      <tbody key={`tbody${props.id}`}>
-        <tr key={`tr${props.id}`}>
-          <td style={{ width: 70 }} key={`tdSets${props.id}`}>{`Sets: ${props.sets}`}</td>
-          <td style={{ width: 80 }} key={`tdReps${props.id}`}>
-            {props.timed ? `Time: ${props.reps}s` : `Reps: ${props.reps}`}
-          </td>
-          <td style={{ width: 100 }} key={`tdWeight${props.id}`}>
-            {!props.timed && `Weight: ${props.weight}kg`}
-          </td>
-          <td key={`tdBreak${props.id}`}>{`Break between sets: ${props.breakTime}s`}</td>
+    <table style={{ fontSize: 12, marginTop: 5, marginBottom: 15 }}>
+      <tbody>
+        <tr>
+          <td style={{ width: 70 }}>{`Sets: ${props.sets}`}</td>
+          <td style={{ width: 80 }}>{props.timed ? `Time: ${props.reps}s` : `Reps: ${props.reps}`}</td>
+          <td style={{ width: 100 }}>{!props.timed && `Weight: ${props.weight}kg`}</td>
+          <td>{`Break between sets: ${props.breakTime}s`}</td>
         </tr>
       </tbody>
     </table>
