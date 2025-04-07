e36bb00d5fe4336ca88e2db1613b2a1e2d20b440
Freddie Schultz <freddieschultz+devacademy@gmail.com>
2025-04-07 16:46:28 +1200

    Add Chakra button

--- client/components/ViewWorkout.tsx
+++ client/components/ViewWorkout.tsx
@@ -3,6 +3,7 @@ import { useDeleteExercise, useWorkoutData } from '../apis/api'
 import ViewWorkoutList from './ViewWorkoutList'
 import { useState } from 'react'
 import ActiveWorkout from './ActiveWorkout'
+import { Button, Flex, HStack } from '@chakra-ui/react'
 
 export default function ViewWorkout() {
   const { id } = useParams()
@@ -28,6 +29,12 @@ export default function ViewWorkout() {
 
   return (
     <>
+      <HStack>
+        <Link to="/workouts">Back</Link>
+        <Button marginRight="auto" marginLeft="30vw" onClick={toggleWorkoutActive}>{`${
+          workoutActive ? 'Stop' : 'Start'
+        } working out!`}</Button>
+      </HStack>
       <div className="flexDiv" style={{ alignItems: 'center' }}>
         <div style={{ width: 400 }}>
           <Link to="/workouts">Back</Link>
