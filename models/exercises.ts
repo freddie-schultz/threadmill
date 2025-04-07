98c0981f1ecfbc8410a989451b3935589ffd2f14
Freddie Schultz <freddieschultz+devacademy@gmail.com>
2025-04-08 02:17:36 +1200

    Start updating models - WIP

--- models/exercises.ts
+++ models/exercises.ts
@@ -6,7 +6,22 @@ export interface Exercise extends ExerciseData {
   id: number
 }
 
+// export interface ExerciseInWorkout {
+//   id: number
+// }
+
+// export interface ExerciseInWorkoutData extends Exercise {
+//   exerciseId: number
+//   workoutId: number
+//   sets: number
+//   reps: number
+//   weight: number
+//   breakTime: number
+//   timed: boolean
+// }
+
 export interface ExerciseInWorkout extends Exercise {
+  exerciseId: number
   workoutId: number
   sets: number
   reps: number
